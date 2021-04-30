d619e279cbfb4044698e0c2b5c7ae4dd6b84a069
ShaluRose <36034406+ShaluRose@users.noreply.github.com>
2021-04-30 20:14:09 +0530

    First Commit
    A simple Weather App in JavaScript

---
+++ main.js
@@ -0,0 +1,36 @@
+const weather = {
+    api: "1f7e4c168d97034654204bec4984f08c",
+    fetchWeather : function(city) {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="
+        +city+
+        "&units=metric&appid="
+        +this.api)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {temp, feels_like, humidity} = data.main;
+        const {icon, description} = data.weather[0];
+        
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".temp").innerText = Math.round(temp);
+        document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png";
+        document.querySelector(".description").innerText= description;
+        document.querySelector(".feel").innerText="Feels like : "+feels_like+"°C";
+        document.querySelector(".humidity").innerText = "Humidity : "+humidity+"%";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Bangalore");
